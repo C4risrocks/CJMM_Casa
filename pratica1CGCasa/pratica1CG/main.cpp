08c3e9ea6775a5f4fdcf9ab018ea6f88f6ab5ba3
C4risrocks <c4risrocks@hotmail.com>
2018-02-23 14:57:25 -0600

    Practica 2
    Mario Bros Pixel Art

--- pratica1CGCasa/pratica1CG/main.cpp
+++ pratica1CGCasa/pratica1CG/main.cpp
@@ -1,82 +1,235 @@
-/* Este programa dibuja una "Tetera", este objeto esta definido
- * en GLUT, se crea una fuente de luz, y un material */
-
-//Incluimos las librerias
-//#include <GL/glut.h>
-//#include <stdlib.h>
+//Semestre 2018 - 2
+//************************************************************//
+//************************************************************//
+//************** Alumno (s): *********************************//
+//*************		Moreno Marín Christian				******//
+//*************											******//
+//************************************************************//
 #include "Main.h"
-
-
-void init(void)
+void InitGL ( GLvoid )     // Inicializamos parametros
 {
-	 // Ubicamos la fuente de luz en el punto (1.0, 1.0, 1.0)
-	 //GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };
-
-	 // Activamos la fuente de luz
-	 glEnable(GL_LIGHTING);
-	 glEnable(GL_LIGHT0);
-
-	 glClearDepth(1.0f);					// Activamos el valor de inicio del buffer de profundidad
-	 glEnable(GL_DEPTH_TEST);				// Hacemos la prueba de profundidad
-	 glDepthFunc(GL_LEQUAL);				// Tipo de prueba de profundidad a hacer
-	 return;
+	//glShadeModel(GL_FLAT);							// Habilitamos Smooth Shading
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);				// Negro de fondo
+	//glClearDepth(1.0f);									// Configuramos Depth Buffer
+	//glEnable(GL_DEPTH_TEST);							// Habilitamos Depth Testing
+	//glDepthFunc(GL_LEQUAL);								// Tipo de Depth Testing a realizar
+	
+	
 }
 
-void reshape(int w, int h)
+void display ( void )   // Creamos la funcion donde se dibuja
 {
-	 if (!h)
-		return;
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	// Limpiamos pantalla y Depth Buffer	
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();									// Reinicializamos la actual matriz Modelview
+	//
+	glPointSize(15);
+	glBegin(GL_POINTS);
+	
+	glColor3f(1, 0, 0);
+	glVertex3f(4, 16, -1.2);
+	glVertex3f(5, 16, -1.2);
+	glVertex3f(6, 16, -1.2);
+	glVertex3f(7, 16, -1.2);
+	glVertex3f(8, 16, -1.2);
 
-	 glViewport(0, 0,  (GLsizei) w, (GLsizei) h);
-	 // Activamos la matriz de proyeccion.
-	 glMatrixMode(GL_PROJECTION);
-	 // "limpiamos" esta con la matriz identidad.
-	 glLoadIdentity();
-	 // Usamos proyeccion ortogonal
-	  //glOrtho(-200, 200, -200, 200, -200, 200);
-	 gluPerspective(30.0f, (GLfloat)800/(GLfloat)600, 0.03, 1000.0);
-	 // Activamos la matriz de modelado/visionado. 
-	 glMatrixMode(GL_MODELVIEW);
-	 // "Limpiamos" la matriz
-	 glLoadIdentity();
-	 return;
+	glVertex3f(3, 15, -1.2);
+	glVertex3f(4, 15, -1.2);
+	glVertex3f(5, 15, -1.2);
+	glVertex3f(6, 15, -1.2);
+	glVertex3f(7, 15, -1.2);
+	glVertex3f(8, 15, -1.2);
+	glVertex3f(9, 15, -1.2);
+	glVertex3f(10, 15, -1.2);
+	glVertex3f(11, 15, -1.2);
+
+	glVertex3f(3, 9, -1.2);
+	glVertex3f(4, 9, -1.2);
+	glVertex3f(6, 9, -1.2);
+	glVertex3f(7, 9, -1.2);
+	
+	glVertex3f(2, 8, -1.2);
+	glVertex3f(3, 8, -1.2);
+	glVertex3f(4, 8, -1.2);
+	glVertex3f(6, 8, -1.2);
+	glVertex3f(7, 8, -1.2);
+	glVertex3f(9, 8, -1.2);
+	glVertex3f(10, 8, -1.2);
+	glVertex3f(11, 8, -1.2);
+
+	glVertex3f(1, 7, -1.2);
+	glVertex3f(2, 7, -1.2);
+	glVertex3f(3, 7, -1.2);
+	glVertex3f(4, 7, -1.2);
+	glVertex3f(9, 7, -1.2);
+	glVertex3f(10, 7, -1.2);
+	glVertex3f(11, 7, -1.2);
+	glVertex3f(12, 7, -1.2);
+	
+	glVertex3f(3, 6, -1.2);
+	glVertex3f(10, 6, -1.2);
+
+
+	glColor3f(0.75, 0.52, 0.42);
+	glVertex3f(6, 14, -1.2);
+	glVertex3f(7, 14, -1.2);
+	glVertex3f(9, 14, -1.2);
+
+	glVertex3f(3, 13, -1.2);
+	glVertex3f(5, 13, -1.2);
+	glVertex3f(6, 13, -1.2);
+	glVertex3f(7, 13, -1.2);
+	glVertex3f(9, 13, -1.2);
+	glVertex3f(10, 13, -1.2);
+	glVertex3f(11, 13, -1.2);
+
+	glVertex3f(3, 12, -1.2);
+	glVertex3f(6, 12, -1.2);
+	glVertex3f(7, 12, -1.2);
+	glVertex3f(8, 12, -1.2);
+	glVertex3f(10, 12, -1.2);
+	glVertex3f(11, 12, -1.2);
+	glVertex3f(12, 12, -1.2);
+
+	glVertex3f(4, 11, -1.2);
+	glVertex3f(5, 11, -1.2);
+	glVertex3f(6, 11, -1.2);
+	glVertex3f(7, 11, -1.2);
+
+	glVertex3f(4, 10, -1.2);
+	glVertex3f(5, 10, -1.2);
+	glVertex3f(6, 10, -1.2);
+	glVertex3f(7, 10, -1.2);
+	glVertex3f(8, 10, -1.2);
+	glVertex3f(9, 10, -1.2);
+	glVertex3f(10, 10, -1.2);
+
+	glVertex3f(1, 6, -1.2);
+	glVertex3f(2, 6, -1.2);
+	glVertex3f(5, 6, -1.2);
+	glVertex3f(8, 6, -1.2);
+	glVertex3f(11, 6, -1.2);
+	glVertex3f(12, 6, -1.2);
+
+	glVertex3f(1, 5, -1.2);
+	glVertex3f(2, 5, -1.2);
+	glVertex3f(3, 5, -1.2);
+	glVertex3f(10, 5, -1.2);
+	glVertex3f(11, 5, -1.2);
+	glVertex3f(12, 5, -1.2);
+
+	glVertex3f(1, 4, -1.2);
+	glVertex3f(2, 4, -1.2);
+	glVertex3f(11, 4, -1.2);
+	glVertex3f(12, 4, -1.2);
+
+
+
+
+
+
+	
+
+	
+	glColor3f(0.4235, 0.2313, 0.1647);
+
+	glVertex3f(3, 14, -1.2);
+	glVertex3f(4, 14, -1.2);
+	glVertex3f(5, 14, -1.2);
+
+	glVertex3f(2, 13, -1.2);
+	glVertex3f(4, 13, -1.2);
+
+	glVertex3f(2, 12, -1.2);
+	glVertex3f(4, 12, -1.2);
+	glVertex3f(5, 12, -1.2);
+	glVertex3f(9, 12, -1.2);
+
+	glVertex3f(2, 11, -1.2);
+	glVertex3f(3, 11, -1.2);
+	glVertex3f(8, 11, -1.2);
+	glVertex3f(9, 11, -1.2);
+	glVertex3f(10, 11, -1.2);
+	glVertex3f(11, 11, -1.2);
+
+	glVertex3f(1, 1, -1.2);
+	glVertex3f(2, 1, -1.2);
+	glVertex3f(3, 1, -1.2);
+	glVertex3f(4, 1, -1.2);
+	glVertex3f(9, 1, -1.2);
+	glVertex3f(10, 1, -1.2);
+	glVertex3f(11, 1, -1.2);
+	glVertex3f(12, 1, -1.2);
+
+	glVertex3f(2, 2, -1.2);
+	glVertex3f(3, 2, -1.2);
+	glVertex3f(4, 2, -1.2);
+	glVertex3f(9, 2, -1.2);
+	glVertex3f(10, 2, -1.2);
+	glVertex3f(11, 2, -1.2);
+
+	glColor3f(0, 0, 1);
+	glVertex3f(3, 3, -1.2);
+	glVertex3f(4, 3, -1.2);
+	glVertex3f(5, 3, -1.2);
+	glVertex3f(8, 3, -1.2);
+	glVertex3f(9, 3, -1.2);
+	glVertex3f(10, 3, -1.2);
+
+	glVertex3f(3, 4, -1.2);
+	glVertex3f(4, 4, -1.2);
+	glVertex3f(5, 4, -1.2);
+	glVertex3f(8, 4, -1.2);
+	glVertex3f(9, 4, -1.2);
+	glVertex3f(10, 4, -1.2);
+
+	glVertex3f(4, 5, -1.2);
+	glVertex3f(5, 5, -1.2);
+	glVertex3f(6, 5, -1.2);
+	glVertex3f(7, 5, -1.2);
+	glVertex3f(8, 5, -1.2);
+	glVertex3f(9, 5, -1.2);
+
+	glVertex3f(4, 6, -1.2);
+	glVertex3f(6, 6, -1.2);
+	glVertex3f(7, 6, -1.2);
+	glVertex3f(9, 6, -1.2);
+
+	glVertex3f(5, 7, -1.2);
+	glVertex3f(6, 7, -1.2);
+	glVertex3f(7, 7, -1.2);
+	glVertex3f(8, 7, -1.2);
+
+	glVertex3f(5, 8, -1.2);
+	glVertex3f(8, 8, -1.2);
+
+	glVertex3f(5, 9, -1.2);
+
+	glEnd();
+
+
+	glFlush();
+
 }
 
-// Aqui ponemos lo que queremos dibujar.
-void display(void)
-{ 
-	 // Propiedades del material
-	 GLfloat mat_ambient[] = { 0.7f, 0.7f, 0.7f, 1.0f };
-	 GLfloat mat_diffuse[] = { 0.1f, 0.5f, 0.8f, 1.0f };
-	 GLfloat mat_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-	 GLfloat mat_shininess[] = { 100.0f };
+void reshape ( int width , int height )   // Creamos funcion Reshape
+{
+	if (height==0)										// Prevenir division entre cero
+	{
+		height=1;
+	}
 
-	 // "Limpiamos" el frame buffer con el color de "Clear", en este 
-	 // caso negro. 
-	 glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glViewport(0,0,width,height);
 
-	 glMatrixMode( GL_MODELVIEW_MATRIX );
-	 glLoadIdentity();
- 
-	 glTranslatef(0.0,0.0,-20.0);
-	 // Rotacion de 30 grados en torno al eje x
-	 glRotated(30.0, 1.0, 0.0, 0.0);
-	 // Rotacion de -30 grados en torno al eje y
-	 //glRotated(-30.0, 0.0, 1.0, 0.0);
-	 glRotated(-30, 0.0, 1.0, 0.0);
-	 // Dibujamos una "Tetera" y le aplico el material
+	glMatrixMode(GL_PROJECTION);						// Seleccionamos Projection Matrix
+	glLoadIdentity();
 
-	 glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
-	 glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
-	 glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-	 glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
-	 glutSolidTeapot(1.0);
-	 //glutWireCube(2.0f);
+	// Ortogonal
+	glOrtho(-10,20,-10,20,0.1,2);
 
-	 //glFlush();
-	 glutSwapBuffers ( );
-	 return;
-} 
+	glMatrixMode(GL_MODELVIEW);							// Seleccionamos Modelview Matrix
+	glLoadIdentity();									
+}
 
 // Termina la ejecucion del programa cuando se presiona ESC
 void keyboard(unsigned char key, int x, int y)
@@ -85,48 +238,23 @@ void keyboard(unsigned char key, int x, int y)
 	 {
 		case 27: exit(0);
 				 break;
-
-		case 'x':
-			//rot += 10;
-			break;
 	 }
 	  glutPostRedisplay();
-	  return;
 }    
 
-// Main del programa.
-int main(int argc, char **argv)
-{ 
-	 // Inicializo OpenGL
-	 glutInit(&argc, argv);
-	 
-	 // Activamos buffer simple y colores del tipo RGB  
-	 //glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB| GLUT_DEPTH);
-	 glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB| GLUT_DEPTH);
+int main ( int argc, char** argv )   // Main Function
+{
+  glutInit            (&argc, argv); // Inicializamos OpenGL
+  glutInitDisplayMode (GLUT_RGBA | GLUT_SINGLE); // Display Mode (Clores RGB y alpha | Buffer Sencillo )
+  glutInitWindowSize  (500, 500);	// Tamaño de la Ventana
+  glutInitWindowPosition (0, 0);	//Posicion de la Ventana
+  glutCreateWindow    ("Practica 2"); // Nombre de la Ventana
+  InitGL ();						// Parametros iniciales de la aplicacion
+  glutDisplayFunc     ( display );  //Indicamos a Glut función de dibujo
+  glutReshapeFunc     ( reshape );	//Indicamos a Glut función en caso de cambio de tamano
+  glutKeyboardFunc	  (keyboard);	//Indicamos a Glut función de manejo de teclado
+  glutMainLoop        ( );          // 
 
-	 // Definimos una ventana de medidas 300 x 300 como ventana 
-	 // de visualizacion en pixels
-	 glutInitWindowSize (300, 300);
-	 
-	 // Posicionamos la ventana en la esquina superior izquierda de 
-	 // la pantalla.
-	 glutInitWindowPosition (0, 0);
-
-	 // Creamos literalmente la ventana y le adjudicamos el nombre que se
-	 // observara en su barra de titulo.
-	 glutCreateWindow ("Tetera");
-
-	 // Inicializamos el sistema 
-	 init();
-
-	 glutDisplayFunc(display); 
-	 glutReshapeFunc(reshape);
-	 glutKeyboardFunc(keyboard);
-	 glutMainLoop();
-	 
-	 // ANSI C requiere que main retorne un valor entero.
-	 return 0;
+  return 0;
 }
-
-
 
