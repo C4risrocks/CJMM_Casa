3a38562f8daea1a8fd24a8841890cb6edeaf9198
C4risrocks <c4risrocks@hotmail.com>
2018-03-02 19:22:29 -0600

    Practica 3
    Prisma con valores variables.

--- pratica1CGCasa/pratica1CG/main.cpp
+++ pratica1CGCasa/pratica1CG/main.cpp
@@ -1,260 +1,223 @@
 //Semestre 2018 - 2
 //************************************************************//
 //************************************************************//
-//************** Alumno (s): *********************************//
-//*************		Moreno Marín Christian				******//
+//************** Alumno (s): Moreno Marín Christian    *******//
+//*************				Visual 2017					******//
 //*************											******//
 //************************************************************//
+
+/*USO DE LA FUNCIÓN prisma(); CON VALOR DADO
+  Se dan los tres valores de (flotante ancho, flotante altura, flotante profundidad)
+  De tal forma que su uso sería void prisma(float ancho, float altura, float profundidad)
+  valorx = ancho
+  valory = altura
+  valorz = profundidad
+  Ejemplo;
+  prisma(2,2,3) //Creará un prima con un ancho de 2 altura de 2 y una profundidad de 3.
+  prisma(1,4,4) //Creará un prima con un ancho de 1 altura de 4 y una profundidad de 4.
+  Se pueden usar valores flotantes.
+  prima(1.5,1.5,3.0) nos dará un prisma con esos valores flotantes.
+*/
+
+//#include <gl/gl.h>     // The GL Header File
+//#include <GL/glut.h>   // The GL Utility Toolkit (Glut) Header
+//#include <stdlib.h>
 #include "Main.h"
+
+float angleX = 0.0f;
+float angleY = 0.0f;
+float angleZ = 0.0f;
+float transX = 0.0f;
+float transY = 0.0f;
+float transZ = -5.0f;
+
+
 void InitGL ( GLvoid )     // Inicializamos parametros
 {
-	//glShadeModel(GL_FLAT);							// Habilitamos Smooth Shading
+
+	//glShadeModel(GL_SMOOTH);							// Habilitamos Smooth Shading
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);				// Negro de fondo
-	//glClearDepth(1.0f);									// Configuramos Depth Buffer
-	//glEnable(GL_DEPTH_TEST);							// Habilitamos Depth Testing
-	//glDepthFunc(GL_LEQUAL);								// Tipo de Depth Testing a realizar
-	
-	
+	glClearDepth(1.0f);									// Configuramos Depth Buffer
+	glEnable(GL_DEPTH_TEST);							// Habilitamos Depth Testing
+	//glEnable(GL_LIGHTING);
+	glDepthFunc(GL_LEQUAL);								// Tipo de Depth Testing a realizar
+	//glEnable ( GL_COLOR_MATERIAL );
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 }
 
-void display ( void )   // Creamos la funcion donde se dibuja
+void prisma(float valorx,float valory, float valorz)
 {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	// Limpiamos pantalla y Depth Buffer	
+	GLfloat vertice [8][3] = {
+				{valorx/2,-valory/2, valorz/2},    //Coordenadas Vértice 0 V0
+				{-valorx/2 ,-valory/2, valorz/2},    //Coordenadas Vértice 1 V1
+				{-valorx/2,-valory/2, -valorz/2},    //Coordenadas Vértice 2 V2
+				{valorx/2 ,-valory/2, -valorz/2},    //Coordenadas Vértice 3 V3
+				{valorx/2 ,valory/2, valorz/2},    //Coordenadas Vértice 4 V4
+				{valorx/2,valory/2, -valorz/2},    //Coordenadas Vértice 5 V5
+				{-valorx/2 ,valory/2, -valorz/2},    //Coordenadas Vértice 6 V6
+				{-valorx/2 ,valory/2, valorz/2},    //Coordenadas Vértice 7 V7
+				};
+
+
+		glBegin(GL_POLYGON);	//Front
+			glColor3f(1.0,0.0,0.0);
+			glVertex3fv(vertice[0]);
+			glVertex3fv(vertice[4]);
+			glVertex3fv(vertice[7]);
+			glVertex3fv(vertice[1]);
+		glEnd();
+
+		glBegin(GL_POLYGON);	//Right
+			glColor3f(0.0,0.0,1.0);
+			glVertex3fv(vertice[0]);
+			glVertex3fv(vertice[3]);
+			glVertex3fv(vertice[5]);
+			glVertex3fv(vertice[4]);
+		glEnd();
+
+		glBegin(GL_POLYGON);	//Back
+			glColor3f(0.0,1.0,0.0);
+			glVertex3fv(vertice[6]);
+			glVertex3fv(vertice[5]);
+			glVertex3fv(vertice[3]);
+			glVertex3fv(vertice[2]);
+		glEnd();
+
+		glBegin(GL_POLYGON);  //Left
+			glColor3f(1.0,1.0,1.0);
+			glVertex3fv(vertice[1]);
+			glVertex3fv(vertice[7]);
+			glVertex3fv(vertice[6]);
+			glVertex3fv(vertice[2]);
+		glEnd();
+
+		glBegin(GL_POLYGON);  //Bottom
+	
+		glColor3f(0.4,0.2,0.6);
+			glVertex3fv(vertice[0]);
+			glVertex3fv(vertice[1]);
+			glVertex3fv(vertice[2]);
+			glVertex3fv(vertice[3]);
+		glEnd();
+
+		glBegin(GL_POLYGON);  //Top
+			glColor3f(0.8,0.2,0.4);
+			glVertex3fv(vertice[4]);
+			glVertex3fv(vertice[5]);
+			glVertex3fv(vertice[6]);
+			glVertex3fv(vertice[7]);
+		glEnd();
+}
+
+void display(void)   // Creamos la funcion donde se dibuja
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	// Limiamos pantalla y Depth Buffer
 	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();									// Reinicializamos la actual matriz Modelview
-	//
-	glPointSize(15);
-	glBegin(GL_POINTS);
+	glLoadIdentity();
+
+	//Poner Código Aquí.
+	glRotatef(angleX, 1, 0, 0);
+	glRotatef(angleY, 0, 1, 0);
+	glTranslatef(transX, transY, transZ);
 	
-	glColor3f(1, 0, 0);
-	glVertex3f(4, 16, -1.2);
-	glVertex3f(5, 16, -1.2);
-	glVertex3f(6, 16, -1.2);
-	glVertex3f(7, 16, -1.2);
-	glVertex3f(8, 16, -1.2);
-
-	glVertex3f(3, 15, -1.2);
-	glVertex3f(4, 15, -1.2);
-	glVertex3f(5, 15, -1.2);
-	glVertex3f(6, 15, -1.2);
-	glVertex3f(7, 15, -1.2);
-	glVertex3f(8, 15, -1.2);
-	glVertex3f(9, 15, -1.2);
-	glVertex3f(10, 15, -1.2);
-	glVertex3f(11, 15, -1.2);
-
-	glVertex3f(3, 9, -1.2);
-	glVertex3f(4, 9, -1.2);
-	glVertex3f(6, 9, -1.2);
-	glVertex3f(7, 9, -1.2);
+	prisma(2.5,1.5,3.0);
 	
-	glVertex3f(2, 8, -1.2);
-	glVertex3f(3, 8, -1.2);
-	glVertex3f(4, 8, -1.2);
-	glVertex3f(6, 8, -1.2);
-	glVertex3f(7, 8, -1.2);
-	glVertex3f(9, 8, -1.2);
-	glVertex3f(10, 8, -1.2);
-	glVertex3f(11, 8, -1.2);
-
-	glVertex3f(1, 7, -1.2);
-	glVertex3f(2, 7, -1.2);
-	glVertex3f(3, 7, -1.2);
-	glVertex3f(4, 7, -1.2);
-	glVertex3f(9, 7, -1.2);
-	glVertex3f(10, 7, -1.2);
-	glVertex3f(11, 7, -1.2);
-	glVertex3f(12, 7, -1.2);
-	
-	glVertex3f(3, 6, -1.2);
-	glVertex3f(10, 6, -1.2);
-
-
-	glColor3f(0.75, 0.52, 0.42);
-	glVertex3f(6, 14, -1.2);
-	glVertex3f(7, 14, -1.2);
-	glVertex3f(9, 14, -1.2);
-
-	glVertex3f(3, 13, -1.2);
-	glVertex3f(5, 13, -1.2);
-	glVertex3f(6, 13, -1.2);
-	glVertex3f(7, 13, -1.2);
-	glVertex3f(9, 13, -1.2);
-	glVertex3f(10, 13, -1.2);
-	glVertex3f(11, 13, -1.2);
-
-	glVertex3f(3, 12, -1.2);
-	glVertex3f(6, 12, -1.2);
-	glVertex3f(7, 12, -1.2);
-	glVertex3f(8, 12, -1.2);
-	glVertex3f(10, 12, -1.2);
-	glVertex3f(11, 12, -1.2);
-	glVertex3f(12, 12, -1.2);
-
-	glVertex3f(4, 11, -1.2);
-	glVertex3f(5, 11, -1.2);
-	glVertex3f(6, 11, -1.2);
-	glVertex3f(7, 11, -1.2);
-
-	glVertex3f(4, 10, -1.2);
-	glVertex3f(5, 10, -1.2);
-	glVertex3f(6, 10, -1.2);
-	glVertex3f(7, 10, -1.2);
-	glVertex3f(8, 10, -1.2);
-	glVertex3f(9, 10, -1.2);
-	glVertex3f(10, 10, -1.2);
-
-	glVertex3f(1, 6, -1.2);
-	glVertex3f(2, 6, -1.2);
-	glVertex3f(5, 6, -1.2);
-	glVertex3f(8, 6, -1.2);
-	glVertex3f(11, 6, -1.2);
-	glVertex3f(12, 6, -1.2);
-
-	glVertex3f(1, 5, -1.2);
-	glVertex3f(2, 5, -1.2);
-	glVertex3f(3, 5, -1.2);
-	glVertex3f(10, 5, -1.2);
-	glVertex3f(11, 5, -1.2);
-	glVertex3f(12, 5, -1.2);
-
-	glVertex3f(1, 4, -1.2);
-	glVertex3f(2, 4, -1.2);
-	glVertex3f(11, 4, -1.2);
-	glVertex3f(12, 4, -1.2);
-
-
-
-
-
-
-	
-
-	
-	glColor3f(0.4235, 0.2313, 0.1647);
-
-	glVertex3f(3, 14, -1.2);
-	glVertex3f(4, 14, -1.2);
-	glVertex3f(5, 14, -1.2);
-
-	glVertex3f(2, 13, -1.2);
-	glVertex3f(4, 13, -1.2);
-
-	glVertex3f(2, 12, -1.2);
-	glVertex3f(4, 12, -1.2);
-	glVertex3f(5, 12, -1.2);
-	glVertex3f(9, 12, -1.2);
-
-	glVertex3f(2, 11, -1.2);
-	glVertex3f(3, 11, -1.2);
-	glVertex3f(8, 11, -1.2);
-	glVertex3f(9, 11, -1.2);
-	glVertex3f(10, 11, -1.2);
-	glVertex3f(11, 11, -1.2);
-
-	glVertex3f(1, 1, -1.2);
-	glVertex3f(2, 1, -1.2);
-	glVertex3f(3, 1, -1.2);
-	glVertex3f(4, 1, -1.2);
-	glVertex3f(9, 1, -1.2);
-	glVertex3f(10, 1, -1.2);
-	glVertex3f(11, 1, -1.2);
-	glVertex3f(12, 1, -1.2);
-
-	glVertex3f(2, 2, -1.2);
-	glVertex3f(3, 2, -1.2);
-	glVertex3f(4, 2, -1.2);
-	glVertex3f(9, 2, -1.2);
-	glVertex3f(10, 2, -1.2);
-	glVertex3f(11, 2, -1.2);
-
-	glColor3f(0, 0, 1);
-	glVertex3f(3, 3, -1.2);
-	glVertex3f(4, 3, -1.2);
-	glVertex3f(5, 3, -1.2);
-	glVertex3f(8, 3, -1.2);
-	glVertex3f(9, 3, -1.2);
-	glVertex3f(10, 3, -1.2);
-
-	glVertex3f(3, 4, -1.2);
-	glVertex3f(4, 4, -1.2);
-	glVertex3f(5, 4, -1.2);
-	glVertex3f(8, 4, -1.2);
-	glVertex3f(9, 4, -1.2);
-	glVertex3f(10, 4, -1.2);
-
-	glVertex3f(4, 5, -1.2);
-	glVertex3f(5, 5, -1.2);
-	glVertex3f(6, 5, -1.2);
-	glVertex3f(7, 5, -1.2);
-	glVertex3f(8, 5, -1.2);
-	glVertex3f(9, 5, -1.2);
-
-	glVertex3f(4, 6, -1.2);
-	glVertex3f(6, 6, -1.2);
-	glVertex3f(7, 6, -1.2);
-	glVertex3f(9, 6, -1.2);
-
-	glVertex3f(5, 7, -1.2);
-	glVertex3f(6, 7, -1.2);
-	glVertex3f(7, 7, -1.2);
-	glVertex3f(8, 7, -1.2);
-
-	glVertex3f(5, 8, -1.2);
-	glVertex3f(8, 8, -1.2);
-
-	glVertex3f(5, 9, -1.2);
-
-	glEnd();
-
-
-	glFlush();
-
+	glutSwapBuffers ( );
+    //glFlush();
+    // Swap The Buffers
 }
 
 void reshape ( int width , int height )   // Creamos funcion Reshape
 {
-	if (height==0)										// Prevenir division entre cero
+  if (height==0)										// Prevenir division entre cero
 	{
 		height=1;
 	}
 
-	glViewport(0,0,width,height);
+	glViewport(0,0,width,height);	
 
 	glMatrixMode(GL_PROJECTION);						// Seleccionamos Projection Matrix
 	glLoadIdentity();
 
-	// Ortogonal
-	glOrtho(-10,20,-10,20,0.1,2);
-
-	glMatrixMode(GL_MODELVIEW);							// Seleccionamos Modelview Matrix
-	glLoadIdentity();									
+	// Tipo de Vista
+	glOrtho(-10,10,-10,10,0.1,20);	
+	//glFrustum (-2,2,-2,1,0.6, 80.0);
 }
 
-// Termina la ejecucion del programa cuando se presiona ESC
-void keyboard(unsigned char key, int x, int y)
+void keyboard ( unsigned char key, int x, int y )  // Create Keyboard Function
 {
-	 switch (key) 
-	 {
-		case 27: exit(0);
-				 break;
-	 }
-	  glutPostRedisplay();
-}    
+	switch ( key ) {
+	
+		case 'w':
+		case 'W':
+			transY +=0.2f;
+			printf("Posicion en Y: %f\n", transY);
+			break;
+		case 's':
+		case 'S':
+			transY -=0.2f;
+			printf("Posicion en Y: %f\n", transY);
+			break;
+		case 'a':
+		case 'A':
+			transX -=0.2f;
+			break;
+		case 'd':
+		case 'D':
+			transX +=0.2f;
+			break;
+		case 27:        // Cuando Esc es presionado...
+			exit ( 0 );   // Salimos del programa
+		break;        
+		default:        // Cualquier otra
+		break;
+	}
+	glutPostRedisplay();
+}
+
+void arrow_keys ( int a_keys, int x, int y )  // Funcion para manejo de teclas especiales (arrow keys)
+{
+  switch ( a_keys ) {
+    case GLUT_KEY_UP:     // Presionamos tecla ARRIBA...
+		angleX += 1.0f;
+		break;
+      //glutFullScreen ( ); // Full Screen Mode
+      //break;
+    case GLUT_KEY_DOWN:               // Presionamos tecla ABAJO...
+		angleX -= 1.0f;
+		break;
+      //glutReshapeWindow ( 500, 500 ); // Cambiamos tamano de Ventana
+      //break;
+	case GLUT_KEY_LEFT:
+		angleY += 1.0f;
+		break;
+	case GLUT_KEY_RIGHT:
+		angleY -= 1.0f;
+		break;
+    default:
+      break;
+  }
+  glutPostRedisplay();
+}
+
 
 int main ( int argc, char** argv )   // Main Function
 {
   glutInit            (&argc, argv); // Inicializamos OpenGL
-  glutInitDisplayMode (GLUT_RGBA | GLUT_SINGLE); // Display Mode (Clores RGB y alpha | Buffer Sencillo )
+  //glutInitDisplayMode (GLUT_RGBA | GLUT_SINGLE | GLUT_DEPTH); // Display Mode (Clores RGB y alpha | Buffer Sencillo )
+  glutInitDisplayMode (GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH); // Display Mode (Clores RGB y alpha | Buffer Doble )
   glutInitWindowSize  (500, 500);	// Tamaño de la Ventana
   glutInitWindowPosition (0, 0);	//Posicion de la Ventana
-  glutCreateWindow    ("Practica 2"); // Nombre de la Ventana
+  glutCreateWindow    ("Practica 3"); // Nombre de la Ventana
   InitGL ();						// Parametros iniciales de la aplicacion
   glutDisplayFunc     ( display );  //Indicamos a Glut función de dibujo
   glutReshapeFunc     ( reshape );	//Indicamos a Glut función en caso de cambio de tamano
-  glutKeyboardFunc	  (keyboard);	//Indicamos a Glut función de manejo de teclado
+  glutKeyboardFunc    ( keyboard );	//Indicamos a Glut función de manejo de teclado
+  glutSpecialFunc     ( arrow_keys );	//Otras
   glutMainLoop        ( );          // 
 
   return 0;
 }
+
+
 
